c9e42de5301f20e6ef061ee2ccea9ec014aa89fa
Patrick Hartford <patrickhartford@gmail.com>
2024-07-10 22:37:11 -0400

    Update APC_UPS_Status.groovy
    Changed Status for contact sensors using HomeKit
    Added time on battery

--- Drivers/APC_UPS_Status.groovy
+++ Drivers/APC_UPS_Status.groovy
@@ -1,7 +1,7 @@
 /*
  *  Copyright 2024
  *
- *  Based on the original work done by 
+ *  Based on the original work done by https://github.com/pghart/Hubitat
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -11,19 +11,24 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
- *
+ *  
  *  APC UPS Status Device for Hubitat and APCUPSD dockerized
+ *  
+ *  Version 1.0 - Gets status for Model, TimeOnBattery, Status, LoadPercent, and TimeLeft 
+ *                Sets the ContactSensor open and closed for HomeKit 
+ *
  *
  */
 
 
 metadata {
-    definition (name: "APC UPS Status Device", namespace: "shrug", author: "Patrick Hartford") {
+    definition (name: "APC UPS Status Device", namespace: "pghart", author: "Patrick Hartford") {
         capability "Refresh"
         capability "Battery"
         capability "ContactSensor"
         
         attribute "Model", "string"
+        attribute "TimeOnBattery", "string"
         attribute "Status", "string"
         attribute "LoadPercent", "number"
 //        attribute "bCharge", "number"
@@ -74,11 +79,11 @@ def fetchAndParseData() {
 }
 
 def parseResult(String result) {
-    def fields = ["MODEL","STATUS", "LOADPCT", "BCHARGE", "TIMELEFT"]
+    def fields = ["MODEL","STATUS", "LOADPCT", "BCHARGE", "TIMELEFT", "TONBATT"]
     def parsedData = [:]
     
     fields.each { field ->
-        def pattern = /${field}\s*:\s*(\S+)/
+        def pattern = (field == "MODEL" || field == "TONBATT") ? /${field}\s*:\s*(.+)/ : /${field}\s*:\s*(\S+)/
         def matcher = result =~ pattern
         if (matcher.find()) {
             parsedData[field] = matcher.group(1)
@@ -93,7 +98,15 @@ def parseResult(String result) {
 
 def updateDeviceAttributes(Map parsedData) {
     if (parsedData.MODEL) sendEvent(name: "Model", value: parsedData.MODEL)
-    if (parsedData.STATUS) sendEvent(name: "Status", value: parsedData.STATUS)
+    if (parsedData.TONBATT) sendEvent(name: "TimeOnBattery", value: parsedData.TONBATT)
+    if (parsedData.STATUS) {
+    sendEvent(name: "Status", value: parsedData.STATUS)
+    logDebug "Updated status: ${parsedData.STATUS}" 
+    // Set ContactSensor based on STATUS
+    def contactState = (parsedData.STATUS == "ONLINE") ? "closed" : "open"
+    sendEvent(name: "contactsensor", value: contactState)
+    logDebug "Updated contact: ${contactState}"
+}
     if (parsedData.LOADPCT) sendEvent(name: "LoadPercent", value: parsedData.LOADPCT as BigDecimal, unit: "%")
     if (parsedData.BCHARGE) {
     def bCharge = (parsedData.BCHARGE as BigDecimal).intValue()
@@ -101,8 +114,7 @@ def updateDeviceAttributes(Map parsedData) {
     logDebug "Updated Battery: ${bCharge}"
     }
     if (parsedData.TIMELEFT) sendEvent(name: "TimeLeft", value: parsedData.TIMELEFT)
-}
-
+    }
 def logDebug(msg) {
     if (logEnable) log.debug(msg)
 }
